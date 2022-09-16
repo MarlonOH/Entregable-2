a52b49d9a0384f3ae886d488f502e8a578082ba2
MarlonOH <111099422+MarlonOH@users.noreply.github.com>
2022-09-15 23:19:35 -0500

    Delete Priimero directory

--- Priimero/Interfaz/InterCarros.cs
+++
@@ -1,9 +0,0 @@
-namespace PRIIMERO.Interfaz;
-
-public interface Interfaz{
-    public void Encender();
-    public void Apagar();
-    public void Detener();
-    public void Movimiento();
-    public void Carrera();
-}
--- Priimero/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using PRIIMERO;
-
-Carro vec = new Carro("McLaren", "2");
-Carro asd = new Carro("Ferraria", "2");
-Carro qda = new Carro("Redbull", "2");
-Circuito cir = new Circuito(6, "Nascar");
-
-cir.Añadir_Carro(vec);
-var dict = cir.Realizar_Prueba();
